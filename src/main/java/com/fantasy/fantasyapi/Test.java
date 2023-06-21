573d337a39da8625c26b98830ec04a687dae0844
Carter Campbell <cjcampbell@burnsmcd.com>
2023-06-21 16:14:36 -0500

    Added update capability and search from database

--- src/main/java/com/fantasy/fantasyapi/Test.java
+++ src/main/java/com/fantasy/fantasyapi/Test.java
@@ -1,42 +1,16 @@
 package com.fantasy.fantasyapi;
+import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.fantasy.fantasyapi.mongo.MongoOperations;
 
-import com.fantasy.fantasyapi.apiCalls.GetAllPlayers;
-import com.fantasy.fantasyapi.apiCalls.GetDraftAdp;
-import com.fantasy.fantasyapi.model.AdpPlayer;
-import com.fantasy.fantasyapi.model.EspnPlayer;
 
 public class Test 
 {
     public static void main(String[] args)
     {
-       GetAllPlayers getAllPlayers = new GetAllPlayers();
-       List<EspnPlayer> filteredList = new ArrayList<EspnPlayer>();
-       filteredList = getAllPlayers.getFilteredPlayerList(150);
-       System.out.println("-----------------------------------------");
-       System.out.println("ESPN Player List");
-       System.out.println("-----------------------------------------");
-       for(EspnPlayer player : filteredList)
-       {
-         System.out.println(player.getEspnName() + " " + player.getPos());
-       }
-       System.out.println("-----------------------------------------");
-       System.out.println("END ESPN Player List");
-       System.out.println("-----------------------------------------");
-       GetDraftAdp getDraftAdp = new GetDraftAdp();
-       List<AdpPlayer> adpList = new ArrayList<AdpPlayer>();
-       adpList = getDraftAdp.getFilteredAdpList(100);
-       System.out.println("-----------------------------------------");
-       System.out.println("ADP Player List");
-       System.out.println("-----------------------------------------");
-       for(AdpPlayer player : adpList)
-       {
-        System.out.println(player.getName() + " " + player.getPosition() + " " + player.getAverageDraftPositionPPR());
-       }
-       System.out.println("-----------------------------------------");
-       System.out.println("END ADP Player List");
-       System.out.println("-----------------------------------------");
+        MongoOperations mongo = new MongoOperations();
+        // mongo.performFantasyEspnUpdate();
+        // Document player = mongo.searchPlayer("fantasyPlayersEspn", "exp", "R");
+        // System.out.println(player.get("espnName") + " " + player.get("exp"));
     }
 }
--- src/main/java/com/fantasy/fantasyapi/mongo/MongoOperations.java
+++ src/main/java/com/fantasy/fantasyapi/mongo/MongoOperations.java
@@ -1,12 +1,19 @@
 package com.fantasy.fantasyapi.mongo;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.bson.Document;
+
+import com.fantasy.fantasyapi.apiCalls.GetAllPlayers;
+import com.fantasy.fantasyapi.apiCalls.GetDraftAdp;
 import com.fantasy.fantasyapi.model.AdpPlayer;
 import com.fantasy.fantasyapi.model.EspnPlayer;
 import com.fantasy.fantasyapi.utility.MongoDbHelper;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -14,34 +21,102 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class MongoOperations 
 {
-    // Method to initialize database connection at a specific endpoint
-    public MongoCollection<Document> dbConnect(String collectionName)
+    // Method to search for a player by any STRING {key, value} pair
+    // returns searched players document
+    public Document searchPlayer(String collectionName, String key, String value)
     {
         // load dotenv attributes (hidden)
         Dotenv dotenv = Dotenv.load();
-
         // use helper class to initialize an object with necessary connection attributes
-        MongoDbHelper dbHelperMain = new MongoDbHelper(dotenv.get("CONNECTION_STRING"), dotenv.get("DATABASE_NAME"), collectionName);
+        MongoDbHelper dbHelperMain = new MongoDbHelper(dotenv.get("CONNECTION_STRING"), dotenv.get("DATABASE_NAME_API"), collectionName);
+        // initialize mongo client using connection string
+        try (MongoClient client = new MongoClient(new MongoClientURI(dbHelperMain.getConnectionString()))) {
+            // initialize database using db name
+            MongoDatabase database = client.getDatabase(dbHelperMain.getDatabaseName());
+            // initialize collection using collection name
+            MongoCollection<Document> collection = database.getCollection(dbHelperMain.getCollectionName());
+            // print success message
+            System.out.println("Connection to " + collectionName + ": SUCCESS");
+            // create document key value pair to search for
+            Document query = new Document(key, value);
+            // search for first player that matches query
+            Document player = collection.find(query).first();
+            client.close();
+            return player;
+        }
+    }
+    
+    // Executes an update on the entire fantasyPlayersEspn collection
+    // "insert" is an up-to-date list pulled directly from API
+    public void performFantasyEspnUpdate()
+    {
+        MongoOperations mongo = new MongoOperations();
+        GetAllPlayers getAllPlayers = new GetAllPlayers();
+        List<EspnPlayer> insert = new ArrayList<EspnPlayer>();
+        insert = getAllPlayers.getFilteredPlayerList(300);
+        // for(EspnPlayer player : insert)
+        // {
+        //     player.setEspnName("test");
+        // }
+        mongo.updateEspnPlayers(insert, "fantasyPlayersEspn");
+    }
+
+    // Executes an update on the entire players collection
+    // "insert" is an up-to-date list pulled directly from API
+    public void performFantasyEspnUpdateAllPlayers()
+    {
+        MongoOperations mongo = new MongoOperations();
+        GetAllPlayers getAllPlayers = new GetAllPlayers();
+        List<EspnPlayer> insert = new ArrayList<EspnPlayer>();
+        String jsonString = getAllPlayers.sendRequestGetAllPlayers();
+        try 
+        {
+            insert = getAllPlayers.mapJsonToPlayerObject(jsonString);
+        } 
+        catch (IOException e) 
+        {
+            System.out.println("Failed to generate allPlayers");
+        }
+        // for(EspnPlayer player : insert)
+        // {
+        //     player.setEspnName("test");
+        // }
+        mongo.updateEspnPlayers(insert, "players");
+    }
+
+    // Executes an update on the entire fantasyPlayersAdp collection
+    // "insert" is an up-to-date list pulled directly from API
+    public void performFantasyAdpUpdate()
+    {
+        MongoOperations mongo = new MongoOperations();
+        GetDraftAdp getDraftAdp = new GetDraftAdp();
+        List<AdpPlayer> insert = new ArrayList<AdpPlayer>();
+        insert = getDraftAdp.getFilteredAdpList(100);
+        // for(AdpPlayer player : insert)
+        // {
+        //     player.setName("test");
+        // }
+        mongo.updateAdpPlayers(insert, "fantasyPlayersAdp");
+    }
+
+    // Method to UPDATE espnPlayer objects in database
+    public void updateEspnPlayers(List<EspnPlayer> updateList, String collectionName) 
+    {
+        // load dotenv attributes (hidden)
+        Dotenv dotenv = Dotenv.load();
+        // use helper class to initialize an object with necessary connection attributes
+        MongoDbHelper dbHelperMain = new MongoDbHelper(dotenv.get("CONNECTION_STRING"), dotenv.get("DATABASE_NAME_API"), collectionName);
         // initialize mongo client using connection string
         try (MongoClient client = new MongoClient(new MongoClientURI(dbHelperMain.getConnectionString()))) {
             // initialize database using db name
             MongoDatabase database = client.getDatabase(dbHelperMain.getDatabaseName());
             // initialize collection using collection name
             MongoCollection<Document> players = database.getCollection(dbHelperMain.getCollectionName());
-            // return document collection to allow CRUD operations
-            return players;
-        }
-    }
-
-    // Method to INSERT espnPlayer objects into database
-    public void insertObjectsEspn(List<EspnPlayer> listToInsert) 
-    {
-            MongoOperations mongo = new MongoOperations();
-            // initialize database connection and document collection "fantasyPlayersEspn" 
-            MongoCollection<Document> players = mongo.dbConnect("fantasyPlayersEspn");
-
-            // iterate through listToInsert and append each attribute to the document
-            for (EspnPlayer player : listToInsert) {
+            // print success message
+            System.out.println("Connection to " + collectionName + ": SUCCESS");
+            List<Document> updateDocuments = new ArrayList<Document>();
+            // iterate through updateList and append each attribute to the document
+            for (EspnPlayer player : updateList) {
                 Document document = new Document("espnID", player.getEspnID())
                         .append("espnName", player.getEspnName())
                         .append("espnIDFull", player.getEspnIDFull())
@@ -66,21 +141,40 @@ public class MongoOperations
                         .append("lastGamePlayed", player.getLastGamePlayed())
                         .append("playerID", player.getPlayerID())
                         .append("exp", player.getExp());
-                // insert the player document into the db collection
-                players.insertOne(document);
-                System.out.println(player.getEspnName() + " added to database!");
+                    updateDocuments.add(document);
             }
+            int counter = 1;
+            for(Document updateDoc : updateDocuments)
+            {
+                Document filter = new Document("espnID", updateDoc.get("espnID"));
+                players.replaceOne(filter, updateDoc);
+                System.out.println(counter + "): " + updateDoc.get("espnName") + " was just updated!");
+                counter++;
+            }
+            System.out.println(collectionName + " updated successfully!");
+            client.close();
+        }
     }
     
-    // method to INSERT adpPlayer objects into database
-    public void insertObjectsAdp(List<AdpPlayer> listToInsert) 
+    // method to UPDATE adpPlayer objects in database
+    public void updateAdpPlayers(List<AdpPlayer> updateList, String collectionName) 
     {
-            MongoOperations mongo = new MongoOperations();
-            // initialize database connection and document collection "fantasyPlayersAdp"
-            MongoCollection<Document> players = mongo.dbConnect("fantasyPlayersAdp");
-
-            // iterate through listToInsert and append each attribute to the document
-            for (AdpPlayer player : listToInsert) {
+        // load dotenv attributes (hidden)
+         Dotenv dotenv = Dotenv.load();
+        // use helper class to initialize an object with necessary connection attributes
+        MongoDbHelper dbHelperMain = new MongoDbHelper(dotenv.get("CONNECTION_STRING"), dotenv.get("DATABASE_NAME_API"), collectionName);
+        // initialize mongo client using connection string
+        try (MongoClient client = new MongoClient(new MongoClientURI(dbHelperMain.getConnectionString()))) {
+            // initialize database using db name
+            MongoDatabase database = client.getDatabase(dbHelperMain.getDatabaseName());
+            // initialize collection using collection name
+            MongoCollection<Document> players = database.getCollection(dbHelperMain.getCollectionName());
+            // print success message
+            System.out.println("Connection to " + collectionName + ": SUCCESS");
+            List<Document> updateDocuments = new ArrayList<Document>();
+            // iterate through updateList and append each attribute to the document
+            for (AdpPlayer player : updateList) 
+            {
                 Document document = new Document("playerID", player.getPlayerID())
                         .append("name", player.getName())
                         .append("team", player.getTeam())
@@ -94,10 +188,19 @@ public class MongoOperations
                         .append("averageDraftPositionRookie", player.getAverageDraftPositionRookie())
                         .append("averageDraftPositionDynasty", player.getAverageDraftPositionDynasty())
                         .append("averageDraftPosition2QB", player.getAverageDraftPosition2QB());
-                // insert the player document into the db collection
-                players.insertOne(document);
-                System.out.println(player.getName() + " added to database!");
+                // insert the player document document list
+                updateDocuments.add(document);
             }
+            int counter = 1;
+            for(Document updateDoc : updateDocuments)
+            {
+                Document filter = new Document("playerID", updateDoc.get("playerID"));
+                players.replaceOne(filter, updateDoc);
+                System.out.println(counter + "): " + updateDoc.get("name") + " was just updated!");
+            }
+            System.out.println(collectionName + " updated successfully!");
+            client.close();
+        }
     }
     
 }
